2d039ab463821b3f32641f24818cdfabfa0eac00
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-03-05 19:03:00 +0100

    @EnableConfigurationProperties(SwaggerProperties.class)

--- src/main/java/com/example/swagger/infrastructure/config/SwaggerProperties.java
+++ src/main/java/com/example/swagger/infrastructure/config/SwaggerProperties.java
@@ -3,6 +3,7 @@ package com.example.swagger.infrastructure.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import javax.validation.constraints.NotBlank;
@@ -12,6 +13,7 @@ import javax.validation.constraints.NotBlank;
  */
 @Configuration
 @ConfigurationProperties("swagger")
+@EnableConfigurationProperties(SwaggerProperties.class)
 @Getter
 @Setter // otherwise exception during boot: Reason: No setter found for property: enabled
 public class SwaggerProperties {
